c9c7c62c919d63b62d0de1e614acf01427953e71
Codelise <claycaluscos@gmail.com>
2026-02-16 09:44:02 +0800

    First commit

---
+++ emeal/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import RecipeDetail from "./pages/RecipeDetail";
+import Favorites from "./pages/Favorites";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ emeal/src/pages/RecipeDetail.jsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+export default function RecipeDetail() {
+  return (
+    <div>
+      <h1>Recipe Detail Page</h1>
+    </div>
+  );
+}
